04ff77cda34ea4e20796f45fa7a6458af05fbf81
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-25 20:36:36 +0600

    .env added to hide username and password

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://tour-master:0KE4COEkMb4pcYwr@cluster0.scfrsgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.scfrsgh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
