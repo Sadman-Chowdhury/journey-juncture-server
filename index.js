6e2c832d11c4a7b65cc9ea8c4fbf5db55a034647
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-27 20:03:19 +0600

    GET added for country data

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ async function run() {
         res.send(result)
     })
 
+    app.get('/country', async(req,res)=>{
+        const cursor = countryCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
     app.get('/touristSpot/:id', async(req,res)=>{
         const id = req.params.id
         const query = {_id: new ObjectId(id)}
