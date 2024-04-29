4f1e1741d3d91cf0d64732819131602917082e3d
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-04-29 17:21:11 +0600

    Some fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const touristSpotCollection = client.db('touristSpotDB').collection('touristSpot')
     const countryCollection = client.db('touristSpotDB').collection('country')
